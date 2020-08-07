165750dfb12f0091ce4d396c4f9719e9ee6809fb
Ahmed <farsalzalam22@gmail.com>
2020-08-07 14:07:24 +0200

    redux and dynamic actions

---
+++ build/precache-manifest.120e047fe206fe53aae7f0b629b43040.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe3e87c2b46130edf03ba67af578469",
+    "url": "/crwn-market/index.html"
+  },
+  {
+    "revision": "ef87193f1bc12ebb4a80",
+    "url": "/crwn-market/static/css/main.5149d924.chunk.css"
+  },
+  {
+    "revision": "6e21678af93a9c091251",
+    "url": "/crwn-market/static/js/2.ecea39ee.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-market/static/js/2.ecea39ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef87193f1bc12ebb4a80",
+    "url": "/crwn-market/static/js/main.393a192a.chunk.js"
+  },
+  {
+    "revision": "a275d72ce43c02459d66",
+    "url": "/crwn-market/static/js/runtime-main.8f886f46.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-market/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,41 +2,32 @@ import React from "react";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.compnent";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUp from "./pages/singin-and-singup/singin-and-singup.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+
+import { setCurrentUser } from "./redux/user/user.action";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: "",
-    };
-  }
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            },
-            () => console.log(this.state.currentUser)
-          );
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
       } else {
-        this.setState({
-          currentUser: userAuth,
-        });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -48,14 +39,28 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInSignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInSignUp />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
-export default App;
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
